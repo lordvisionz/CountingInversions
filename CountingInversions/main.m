ec3c56cddcb18c79e741fdf2a50dc1ac610fd6f0
Abhishek Moothedath <lord.visionz@gmail.com>
2015-07-19 17:06:11 -0400

    initial checkin

---
+++ CountingInversions/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CountingInversions
+//
+//  Created by Abhi on 7/19/15.
+//  Copyright (c) 2015 ___Abhishek Moothedath___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
